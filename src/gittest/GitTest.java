2d9becd4dd32407b002287f474bb1b8dc6e578a3
Panupong Nakmanee <panupong.n@mail.kmutt.ac.th>
2020-10-21 23:38:41 +0700

    feature1

---
+++ src/gittest/GitTest.java
@@ -0,0 +1,13 @@
+package gittest;
+public class GitTest {
+
+    
+    
+    public static void main(String[] args) {
+        sayHello();
+    }
+    public static void sayHello(){
+        System.out.println("Hello");
+    }
+    
+}
